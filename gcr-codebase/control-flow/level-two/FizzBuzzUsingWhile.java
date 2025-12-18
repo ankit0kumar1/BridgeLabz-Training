ce6ec0a2b641f585f6fdc205c9195df738506633
anikit0kumar1 <ankit620580@gmail.com>
2025-12-19 01:10:50 +0530

    [Ankit Kumar]: Added a program to print FizzBuzz using while loop.

---
+++ gcr-codebase/control-flow/level-two/FizzBuzzUsingWhile.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class FizzBuzzUsingWhile {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Taking input from the user
+        System.out.print("Enter a positive integer: ");
+        int number = input.nextInt();
+
+        // Check for positive integer
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            int i = 1;
+
+            // FizzBuzz using while loop
+            while (i <= number) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
+                i++;
+            }
+        }
+
+        input.close();
+    }
+}
