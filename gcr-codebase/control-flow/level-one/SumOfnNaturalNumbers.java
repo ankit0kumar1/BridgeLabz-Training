8a52bc019e82443a671a3b00bf2e7a48b384e186
anikit0kumar1 <ankit620580@gmail.com>
2025-12-18 18:59:26 +0530

    [Ankit Kumar]: Added a program to find sum of n natural numbers using while loop.

---
+++ gcr-codebase/control-flow/level-one/SumOfnNaturalNumbers.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class SumOfnNaturalNumbers{
+	public static void main(String [] args){
+		Scanner input = new Scanner(System.in);
+		
+		//get input form the user.
+		System.out.print("Enter a number: ");
+		int number = input.nextInt();
+		
+		if (number > 0){
+		
+		//Formula to calculate the sum of n natural numbers
+		int sumFormula = number * (number + 1) / 2;
+		int sumWhile =0;
+		
+		//While loop to calculate sum of n natural numbers.
+		while(number != 0){
+		sumWhile+=number;
+		number--;
+		}
+		
+		System.out.println("Sum using while loop: " + sumWhile);
+        System.out.println("Sum using formula: " + sumFormula);
+		
+		//Comparing the results 
+			if (sumWhile == sumFormula) {
+                System.out.println("Both gives same result.");
+            } else {
+                System.out.println("The result is not same.");
+            }
+			
+			
+		}else{
+			System.out.println("The number " + number + " is not a natural number.");
+		}
+		input.close();
+	}
+}
