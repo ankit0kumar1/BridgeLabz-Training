ae3bd09ad6190c503cffe866a79ed804bd052752
anikit0kumar1 <ankit620580@gmail.com>
2025-12-20 14:58:43 +0530

    [Ankit Kumar] : Added a program to find Largest and second largest.

---
+++ gcr-codebase/arrays/level-two/LargestAndSecondLargest.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class LargestAndSecondLargest{
+    public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+
+        // Taking input form the user
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int maxDigit = 10;
+        int[] digits = new int[maxDigit];
+        int index = 0;
+
+        // Store digits in array
+        while (number != 0 && index < maxDigit) {
+            digits[index] = number % 10;
+            number = number / 10;
+            index++;
+        }
+
+        int largest = 0;
+        int secondLargest = 0;
+
+        // Find largest and second largest digit
+        for (int i = 0; i < index; i++) {
+            if (digits[i] > largest) {
+                secondLargest = largest;
+                largest = digits[i];
+            } else if (digits[i] > secondLargest && digits[i] != largest) {
+                secondLargest = digits[i];
+            }
+        }
+
+        // Displaying the result
+        System.out.println("Largest digit: " + largest);
+        System.out.println("Second largest digit: " + secondLargest);
+
+        input.close();
+    }
+}
