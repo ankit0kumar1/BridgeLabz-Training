bfa5e85b77d0d7e37afa459cd45b49cd2f094829
anikit0kumar1 <ankit620580@gmail.com>
2025-12-22 14:45:56 +0530

    [Ankit Kumar] : Added a program to handle StringIndexOutOfBoundsException.

---
+++ gcr-codebase/string/level-one/StringIndexOutOfBoundsExceptionDemo.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class StringIndexOutOfBoundsExceptionDemo{
+
+	//Method to generate exception 
+	public static void generateException(String text){
+		
+        System.out.println(text.charAt(text.length())); // Exception occurs here
+	}
+	// method to demostrate  StringIndexOutOfBoundsException
+	public static void handleException(String text){
+		try{
+			System.out.println(text.charAt(text.length()));
+		}catch(StringIndexOutOfBoundsException e){
+			System.out.println("StringIndexOutOfBoundsException caught and handled!");
+		}
+	}
+	
+	
+	public static void main(String [] args){
+		Scanner input = new Scanner(System.in);
+		
+		//Take input form the user
+		System.out.print("Enter a text : ");
+		String text = input.next();
+		
+		// 1. Calling method that generates exception
+        System.out.println("Generating NullPointerException:");
+       // generateException(text);
+
+        // 2. Refactored code – handling the exception
+        System.out.println("\nHandling NullPointerException:");
+        handleException(text);
+		
+	}
+}
