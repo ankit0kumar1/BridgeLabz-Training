a4c89e456731b4e83f7121ee20f3ae210e3052c4
anikit0kumar1 <ankit620580@gmail.com>
2025-12-29 10:13:39 +0530

    [Ankit Kumar] : Added Student program.

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/consturctors/level1/Student.java
@@ -0,0 +1,22 @@
+package com.consturctors.level1;
+
+public class Student {
+	public int rollnumber;
+	protected String name;
+	private double cgpa;
+	
+	public void setCgpa(double cgpa) {
+		this.cgpa = cgpa;
+		
+	}
+	public double getCgpa() {
+		return cgpa;
+	}
+	Student(int rollnumber, String name, double cgpa ){
+		this.rollnumber = rollnumber;
+		this.name = name;
+		this.cgpa = cgpa;
+		
+	}
+	
+}
