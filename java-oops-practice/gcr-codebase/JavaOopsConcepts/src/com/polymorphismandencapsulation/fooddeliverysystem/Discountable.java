d28a2d7397d86b0221cd25682277344dd8aed75d
anikit0kumar1 <ankit620580@gmail.com>
2026-01-02 16:03:49 +0530

    [Ankit Kumar] : Added Online Food Delivery System programs.

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/polymorphismandencapsulation/fooddeliverysystem/Discountable.java
@@ -0,0 +1,10 @@
+package com.polymorphismandencapsulation.fooddeliverysystem;
+
+public interface Discountable {
+
+	// Apply discount on total price
+	double applyDiscount();
+
+	// Get discount details
+	String getDiscountDetails();
+}
