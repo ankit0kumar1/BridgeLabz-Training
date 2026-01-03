8380733fcfb990458cb347c328c59c8f117fb470
anikit0kumar1 <ankit620580@gmail.com>
2026-01-03 22:48:34 +0530

    [Ankit Kumar] : Added My Bank Program .

---
+++ java-oops-practice/scenario-based/oopspracticescenariobasedquestions/src/com/mybank/MyBankApp.java
@@ -0,0 +1,83 @@
+package com.mybank;
+
+import java.util.*;
+
+
+
+public class MyBankApp {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Welcome to MyBank");
+		System.out.println("-----------------");
+		
+		//choose account type
+		System.out.println("Select Account Type");
+		System.out.println("1. Savings Account");
+		System.out.println("2. Current Account");
+		int choice = input.nextInt();
+		input.nextLine();//consume nextLine
+		
+		//Account number
+		System.out.println("Enter Account Number: ");
+		String accountNumber = input.nextLine();
+		
+		//Opening balance
+		System.out.println("Enter Opening Balance: ");
+		double openingBalance = input.nextDouble();
+		
+		Account account;
+		
+		if(choice ==1) {
+			//savings account
+			System.out.println("Enter Interest Rate(%): ");
+			double interestRate = input.nextDouble();
+			
+			account = new SavingsAccount(accountNumber, openingBalance, interestRate);
+		}else if(choice ==2) {
+			//current account
+			account = new CurrentAccount(accountNumber, openingBalance);
+		}else {
+			System.out.println("Invalid Account type. Exiting");
+			input.close();
+			return;
+		}
+		int option;
+		do {
+			System.out.println("\nChoose Option:");
+			System.out.println("1. Deposit\n2. Withdraw\n3. Check Balance\n4. Calculate Interest\n5. Exit");
+			option = input.nextInt();
+			
+			switch(option) {
+				case 1 : 
+					System.out.println("Enter the amount to deposit: ");
+					account.deposit(input.nextDouble());
+					break;
+					
+				case 2 :
+					System.out.println("Enter the amount to withdraw");
+					account.withdraw(input.nextDouble());
+					break;
+					
+				case 3 : 
+					System.out.println("Current Balance: " + account.checkBalance());
+					break;
+					
+				case 4 :
+					System.out.println("Calculated Intrest: " + account.calculateInterest());
+					break;
+					
+				case 5 : 
+					System.out.println("Thank you for using MyBank.");
+					break;
+				
+				default:
+					System.out.println("Invalid option.");
+			}
+		}while(option != 5);
+		
+		input.close();
+		
+		
+	}
+}
