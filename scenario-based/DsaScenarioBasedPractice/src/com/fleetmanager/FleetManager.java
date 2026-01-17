17b2bdfcafcba65261c25e1b63bc99bee2bd1dd1
anikit0kumar1 <ankit620580@gmail.com>
2026-01-17 15:10:33 +0530

    [Ankit Kumar] : Modified FleetManager program.

--- scenario-based/DsaScenarioBasedPractice/src/com/fleetmanager/FleetManager.java
+++ scenario-based/DsaScenarioBasedPractice/src/com/fleetmanager/FleetManager.java
@@ -2,55 +2,64 @@ package com.fleetmanager;
 
 class FleetManager {
 
-    public static void mergeSort(int[] mileage, int left, int right) {
+    public static void mergeSort(Vehicle[] vehicles, int left, int right) {
         if (left < right) {
             int mid = (left + right) / 2;
 
-            mergeSort(mileage, left, mid);
-            mergeSort(mileage, mid + 1, right);
+            mergeSort(vehicles, left, mid);
+            mergeSort(vehicles, mid + 1, right);
 
-            merge(mileage, left, mid, right);
+            merge(vehicles, left, mid, right);
         }
     }
 
-    private static void merge(int[] mileage, int left, int mid, int right) {
+    private static void merge(Vehicle[] vehicles, int left, int mid, int right) {
+
         int n1 = mid - left + 1;
         int n2 = right - mid;
 
-        int[] leftArr = new int[n1];
-        int[] rightArr = new int[n2];
+        Vehicle[] leftArr = new Vehicle[n1];
+        Vehicle[] rightArr = new Vehicle[n2];
 
         for (int i = 0; i < n1; i++)
-            leftArr[i] = mileage[left + i];
+            leftArr[i] = vehicles[left + i];
         for (int j = 0; j < n2; j++)
-            rightArr[j] = mileage[mid + 1 + j];
+            rightArr[j] = vehicles[mid + 1 + j];
 
         int i = 0, j = 0, k = left;
 
-        // Merge sorted arrays
+        // Merge based on mileage
         while (i < n1 && j < n2) {
-            if (leftArr[i] <= rightArr[j]) {
-                mileage[k++] = leftArr[i++];
+            if (leftArr[i].mileage <= rightArr[j].mileage) {
+                vehicles[k++] = leftArr[i++];
             } else {
-                mileage[k++] = rightArr[j++];
+                vehicles[k++] = rightArr[j++];
             }
         }
 
         while (i < n1)
-            mileage[k++] = leftArr[i++];
+            vehicles[k++] = leftArr[i++];
 
         while (j < n2)
-            mileage[k++] = rightArr[j++];
+            vehicles[k++] = rightArr[j++];
     }
 
     public static void main(String[] args) {
-        int[] vehicles = {12000, 15000, 18000, 11000, 14000, 20000};
 
-        mergeSort(vehicles, 0, vehicles.length - 1);
+        Vehicle[] fleet = {
+            new Vehicle("V101", 12000),
+            new Vehicle("V102", 15000),
+            new Vehicle("V103", 18000),
+            new Vehicle("V104", 11000),
+            new Vehicle("V105", 14000),
+            new Vehicle("V106", 20000)
+        };
 
-        for (int km : vehicles) {
-            System.out.print(km + " ");
+        mergeSort(fleet, 0, fleet.length - 1);
+
+        System.out.println("Vehicle Maintenance Schedule (by mileage):");
+        for (Vehicle v : fleet) {
+            System.out.println(v.vehicleId + " -> " + v.mileage);
         }
     }
 }
-
