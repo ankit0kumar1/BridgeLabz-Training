c7db1d0895e733c78db97780800e920ec7f3f2bf
anikit0kumar1 <ankit620580@gmail.com>
2026-01-17 15:12:27 +0530

    [Ankit Kumar] : Added Vehicle class in FleetManager program.

---
+++ scenario-based/DsaScenarioBasedPractice/src/com/fleetmanager/Vehicle.java
@@ -0,0 +1,12 @@
+package com.fleetmanager;
+
+class Vehicle {
+    String vehicleId;
+    int mileage;
+
+    Vehicle(String vehicleId, int mileage) {
+        this.vehicleId = vehicleId;
+        this.mileage = mileage;
+    }
+}
+
