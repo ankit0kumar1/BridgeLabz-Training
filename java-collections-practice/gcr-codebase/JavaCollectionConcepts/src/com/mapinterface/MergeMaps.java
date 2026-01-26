8357210328b7a5b3d0c08e88c7c03e787986e726
anikit0kumar1 <ankit620580@gmail.com>
2026-01-26 17:54:13 +0530

    [Ankit Kumar] : Added Merge maps program.

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/mapinterface/MergeMaps.java
@@ -0,0 +1,30 @@
+package com.mapinterface;
+
+import java.util.*;
+
+public class MergeMaps {
+    public static void main(String[] args) {
+
+        Map<String, Integer> map1 = new HashMap<>();
+        map1.put("A", 1);
+        map1.put("B", 2);
+
+        Map<String, Integer> map2 = new HashMap<>();
+        map2.put("B", 3);
+        map2.put("C", 4);
+
+        Map<String, Integer> result = mergeMaps(map1, map2);
+
+        System.out.println(result);  // {A=1, B=5, C=4}
+    }
+
+    public static Map<String, Integer> mergeMaps(Map<String, Integer> map1, Map<String, Integer> map2) {
+        Map<String, Integer> merged = new HashMap<>(map1);
+
+        for (Map.Entry<String, Integer> entry : map2.entrySet()) {
+            merged.merge(entry.getKey(), entry.getValue(), Integer::sum);
+        }
+
+        return merged;
+    }
+}
