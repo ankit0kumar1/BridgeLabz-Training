fccc3e2909d56fd1e14ad4e695c45bb641bd76cd
anikit0kumar1 <ankit620580@gmail.com>
2026-01-27 07:11:38 +0530

    [Ankit Kumar] : Added Read File program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/exception/ReadFile.java
@@ -0,0 +1,21 @@
+package com.exception;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+
+        try (FileInputStream fis = new FileInputStream("data.txt")) {
+
+            int ch;
+            while ((ch = fis.read()) != -1) {
+                System.out.print((char) ch);
+            }
+
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+}
+
