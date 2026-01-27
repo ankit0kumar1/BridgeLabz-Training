02834e1b02c0c1dd861d683e5d240e4a7e537229
anikit0kumar1 <ankit620580@gmail.com>
2026-01-27 15:57:42 +0530

    [Ankit Kumar] : Added  MathUtils.java program

---
+++ java-collections-practice/gcr-codebase/junitpractice/src/main/java/com/junitpractice/MathUtils.java
@@ -0,0 +1,11 @@
+package com.junitpractice;
+
+public class MathUtils {
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
