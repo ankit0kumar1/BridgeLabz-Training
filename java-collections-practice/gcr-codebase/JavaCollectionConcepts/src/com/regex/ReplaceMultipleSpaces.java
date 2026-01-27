51e49e2f76bc630d817d5559ccfdd6406c763fb1
anikit0kumar1 <ankit620580@gmail.com>
2026-01-27 11:46:04 +0530

    [Ankit Kumar] : Added Replace Mulitple Spaces program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/regex/ReplaceMultipleSpaces.java
@@ -0,0 +1,13 @@
+package com.regex;
+
+public class ReplaceMultipleSpaces {
+    public static void main(String[] args) {
+        String input = "This   is   an   example   with    multiple   spaces.";
+
+        String output = input.replaceAll("\\s+", " ");
+
+        System.out.println("Input:  " + input);
+        System.out.println("Output: " + output);
+    }
+}
+
