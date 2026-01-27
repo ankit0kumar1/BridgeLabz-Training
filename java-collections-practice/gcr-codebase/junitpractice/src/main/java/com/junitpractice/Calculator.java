9667e4f221946d68f653e09febb25846e23fa6de
anikit0kumar1 <ankit620580@gmail.com>
2026-01-27 15:20:57 +0530

    [Ankit Kumar] : Added Calculator.java file

---
+++ java-collections-practice/gcr-codebase/junitpractice/src/main/java/com/junitpractice/Calculator.java
@@ -0,0 +1,20 @@
+package com.junitpractice;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b; // will automatically throw ArithmeticException if b == 0
+    }
+}
