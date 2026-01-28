50db38cc2e2b02e80cca56a2058b1c78c7a7708b
anikit0kumar1 <ankit620580@gmail.com>
2026-01-28 10:14:37 +0530

    [Ankit Kumar] : Added a program for override annotation

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/annotations/override/Dog.java
@@ -0,0 +1,10 @@
+package com.annotations.override;
+
+public class Dog extends Animal {
+
+    @Override
+    public void makeSound() {
+        System.out.println("Dog barks: Woof Woof!");
+    }
+}
+
---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/annotations/override/Main.java
@@ -0,0 +1,10 @@
+package com.annotations.override;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.makeSound();
+    }
+}
+
