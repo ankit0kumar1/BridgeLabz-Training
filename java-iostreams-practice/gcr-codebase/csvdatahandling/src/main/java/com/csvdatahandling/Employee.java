9fbb444bce6895cddd6732c7312ab6726fb8cc0f
anikit0kumar1 <ankit620580@gmail.com>
2026-01-30 11:17:44 +0530

    [Ankit Kumar] : Added Employee.java file.

---
+++ java-iostreams-practice/gcr-codebase/csvdatahandling/src/main/java/com/csvdatahandling/Employee.java
@@ -0,0 +1,32 @@
+package com.csvdatahandling;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+}
+
