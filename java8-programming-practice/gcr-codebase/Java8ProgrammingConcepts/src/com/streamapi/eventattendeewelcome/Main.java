38f2bafab99d2d2e02b239470c5872486c824b3a
anikit0kumar1 <ankit620580@gmail.com>
2026-02-03 15:27:18 +0530

    [Ankit Kumar] : Added Event Attendee Welcome message.

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/streamapi/eventattendeewelcome/Main.java
@@ -0,0 +1,12 @@
+package com.streamapi.eventattendeewelcome;
+
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		List<String> names = Arrays.asList("Ankit", "Aryan", "Keshav", "Ram", "Sanchit");
+		
+		names.forEach(n -> System.out.println("Welcome " + n));
+		
+		
+	}
+}
