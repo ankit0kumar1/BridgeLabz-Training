9506bddf0f380ddaa585eeb70e7f96773924dd8a
anikit0kumar1 <ankit620580@gmail.com>
2026-02-05 07:38:11 +0530

    [Ankit Kumar] : Added Student Library Management system program

---
+++ java-design-prattern-practice/gcr-codebase/JavaDesignPatternConcepts/src/com/studentlibrarysystem/Librarian.java
@@ -0,0 +1,13 @@
+package com.studentlibrarysystem;
+
+public class Librarian extends User {
+
+    public Librarian(String name) {
+        super(name);
+    }
+
+    @Override
+    public void getRole() {
+        System.out.println(name + " is Librarian (manages inventory)");
+    }
+}
---
+++ java-design-prattern-practice/gcr-codebase/JavaDesignPatternConcepts/src/com/studentlibrarysystem/Observer.java
@@ -0,0 +1,5 @@
+package com.studentlibrarysystem;
+
+public interface Observer {
+	void update(String message);
+}
